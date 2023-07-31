d728af815d33348b027b72027ec2928eede1f1f7
Hakan Kurt <hakankurt@Hakan-MacBook-Pro.local>
2023-07-31 23:18:19 +0300

    Initial commit

---
+++ composite-product/src/main/java/com/hakan/microservices/composite/product/CompositeProductApplication.java
@@ -0,0 +1,13 @@
+package com.hakan.microservices.composite.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeProductApplication.class, args);
+	}
+
+}
---
+++ composite-product/src/test/java/com/hakan/microservices/composite/product/CompositeProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakan.microservices.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
